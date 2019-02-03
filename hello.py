e5d18890db20118bf3fa605020187d6f5988ab35
Harris Beg <legostarwarsguy02@gmail.com>
2019-02-02 19:09:41 -0800

    Added images + loader

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template; import os
     
 app = Flask(__name__)
 settings = {
@@ -6,9 +6,11 @@ settings = {
 		"description" : "Hello"
 	}
 }
+
 @app.route('/')
 def home():
-	return render_template('home.html', name="Home", description = settings["Home"]["description"])
+	images = os.listdir(os.path.join(app.static_folder, "assets"))
+	return render_template('home.html', name="Home", description = settings["Home"]["description"], images=images)
 
 if __name__ == '__main__':
 	app.run(debug=True)
