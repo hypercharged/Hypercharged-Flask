ffb4903900d4a1949cb47265b841b1c271514d54
Harris Beg <legostarwarsguy02@gmail.com>
2019-02-02 19:48:43 -0800

    Removed need for indexes

--- hello.py
+++ hello.py
@@ -10,6 +10,9 @@ settings = {
 @app.route('/')
 def home():
 	images = os.listdir(os.path.join(app.static_folder, "assets"))
+	for image in images:
+		if ("IMG" not in image):
+			images.remove(image)
 	return render_template('home.html', name="Home", description = settings["Home"]["description"], images=images)
 
 if __name__ == '__main__':
