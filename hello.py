7573579e0724e4630aa0f832b4a9a291387ee8ef
Danial <danny00b@gmail.com>
2019-02-03 18:25:49 -0800

    Updated hello.py

--- hello.py
+++ hello.py
@@ -38,7 +38,7 @@ def home():
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
-                          'favicon.ico',mimetype='image/vnd.microsoft.icon'
+                          'favicon.ico',mimetype='image/vnd.hypercharged.icon'
 
 if __name__ == '__main__':
 	app.run(debug=True)
