ca4df1b66b6ecfeb24dd0b4c3a8323576264c563
Danial <danny00b@gmail.com>
2019-02-04 21:34:34 -0800

    Python Packages

--- frontend/main.js
+++ frontend/main.js
@@ -29,3 +29,29 @@ function configureFirebaseLoginWidget() {
   var ui = new firebaseui.auth.AuthUI(firebase.auth());
   ui.start('#firebaseui-auth-container', uiConfig);
 }
+
+irebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    $('#logged-out').hide();
+    var name = user.displayName;
+
+    /* If the provider gives a display name, use the name for the
+    personal welcome message. Otherwise, use the user's email. */
+    var welcomeName = name ? name : user.email;
+
+    user.getToken().then(function(idToken) {
+      userIdToken = idToken;
+
+      /* Now that the user is authenicated, fetch the notes. */
+      fetchNotes();
+
+      $('#user').text(welcomeName);
+      $('#logged-in').show();
+
+    });
+
+  } else {
+    $('#logged-in').hide();
+    $('#logged-out').show();
+
+  }
