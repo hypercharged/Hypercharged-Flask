6923dc0e62cc4ba7a4904b7722094a724e9b5f95
Danial <danny00b@gmail.com>
2019-02-04 21:18:33 -0800

    Updated front end to include login from various other platforms
    Users will now be able to sign in with their Facebook accounts, Google accounts, etc

--- hello.py
+++ hello.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template;
 import os, json; 
 from flask_sitemap import Sitemap;
 from flask import send_from_directory
+import pyrebase
 
 
 app = Flask(__name__)
@@ -13,6 +14,16 @@ settings = {
 	}
 }
 app.config['SITEMAP_INCLUDE_RULES_WITHOUT_PARAMS']=True
+
+#service = "https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword"
+#var config = {
+#  apiKey: "AIzaSyC-6Pi--GB4V4RaWP_k419EsEwqssfULSc",
+#  authDomain: "hypercharged-website.firebaseapp.com",
+#  databaseURL: "https://hypercharged-website.firebaseio.com",
+#  projectId: "hypercharged-website",
+#  storageBucket: "ypercharged-website.appspot.com",
+#  messagingSenderId: "<MESSAGING_SENDER_ID>"
+#}
 
 def retrieveMetaData():
 	with open('config.json') as f:
