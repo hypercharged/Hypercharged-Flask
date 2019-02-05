64cfaac6242270431a28691bd160cc9f9b23946a
Harris Beg <legostarwarsguy02@gmail.com>
2019-02-04 20:16:21 -0800

    Keypair added + New classes

---
+++ Shop/Prices.py
@@ -0,0 +1,5 @@
+from enum import Enum
+class Prices(Enum):
+	ITEM_1 = 1.00
+	ITEM_2 = 1.50
+	ITEM_3 = 2.00
--- hello.py
+++ hello.py
@@ -1,46 +1,58 @@
 from flask import Flask, render_template, send_from_directory, request; 
-import os, json, stripe; 
+import os, json, stripe, pickle; 
 from flask_sitemap import Sitemap;
+from Shop.Wallpaper import Wallpaper
 
 app = Flask(__name__)
 smp = Sitemap(app=app)
+PICKLE_FILE = "keypair.hypercharged"
 carEvents = []
 settings = {
-	"Home": {
-		"description" : "Hello"
-	}
+    "Home": {
+        "description" : "Hello"
+    }
 }
-if (os.environ.get("SECRET_KEY") == None):
-	secret = input("SECRET KEY: "); publishable = input("PUBLISHABLE KEY: ")
-	os.environ["SECRET_KEY"] = secret; os.environ["PUBLISHABLE_KEY"] = publishable
+secret, publish = "",""
+"""
+
+KEY FOR DEVS: DO NOT, UNDER ANY CIRCUMSTANCE, LEAK SECRET OR PUBLISHABLE KEYS --> Stored in keypair.hypercharged file
+
+"""
+try:
+    pick = pickle.load(open(PICKLE_FILE, 'rb'))
+    publish = pick["PUBLISHABLE_KEY"]
+    secret = pick["SECRET_KEY"]
+except:
+    secret = input("SECRET KEY: "); publish = input("PUBLISHABLE KEY: ")
+pickle.dump({"PUBLISHABLE_KEY": publish, "SECRET_KEY":secret}, open(PICKLE_FILE,'wb'))
 stripe_keys = {
-  'secret_key': os.environ['SECRET_KEY'],
-  'publishable_key': os.environ['PUBLISHABLE_KEY']
+  'secret_key': secret,
+  'publishable_key': publish
 }
 stripe.api_key = stripe_keys['secret_key']
 app.config['SITEMAP_INCLUDE_RULES_WITHOUT_PARAMS']=True
 
 def retrieveMetaData():
-	with open('config.json') as f:
-		jsonF = json.loads(f.read());
-		return jsonF
+    with open('config.json') as f:
+        jsonF = json.loads(f.read());
+        return jsonF
 def getImagesCarEvents():
-	with open('config.json') as f:
-		jsonF = json.loads(f.read())
-		for key,value in jsonF:
-			if (value["event"] not in carEvents):
-				carEvents["events"].append(value["event"])
-			carEvents["images"][value["event"]].append(key)
+    with open('config.json') as f:
+        jsonF = json.loads(f.read())
+        for key,value in jsonF:
+            if (value["event"] not in carEvents):
+                carEvents["events"].append(value["event"])
+            carEvents["images"][value["event"]].append(key)
 
 @app.route('/')
 def home():
-	images = os.listdir(os.path.join(app.static_folder, "assets"))
-	metadata = retrieveMetaData()
-	for image in images:
-		if ("IMG" not in image):
-			images.remove(image)
-		print(image)
-	return render_template('home.html', name="Home", description = settings["Home"]["description"], images=images, metadata=metadata)
+    images = os.listdir(os.path.join(app.static_folder, "assets"))
+    metadata = retrieveMetaData()
+    for image in images:
+        if ("IMG" not in image):
+            images.remove(image)
+        print(image)
+    return render_template('home.html', name="Home", description = settings["Home"]["description"], images=images, metadata=metadata)
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
@@ -48,14 +60,13 @@ def favicon():
 
 @app.route('/buy')
 def buy():
-	return render_template('buy.html', name="Buy", description = settings["Home"]["description"], key = stripe_keys["publishable_key"])
+    return render_template('buy.html', name="Buy", description = settings["Home"]["description"], key = stripe_keys["publishable_key"])
 @app.route('/charge', methods=['POST'])
 def charge():
     # Amount in cents
-    amount = 99999999
-
+    amount  = 100
     customer = stripe.Customer.create(
-        email='customer@example.com', #put user email here
+        email="example@example.com", #put user email here
         source=request.form['stripeToken']
     )
 
@@ -66,13 +77,14 @@ def charge():
         description='Flask Charge'
     )
     product = stripe.Product.create(
-		  name='Hypercharged Wallpaper',
-		  type='good',
-		  attributes=['download_url'],
-		  description='High-quality wallpaper straight from the source',
-		)
+        name='Hypercharged Wallpaper',
+        type='good',
+        attributes=['download_url'],
+        description='High-quality wallpaper straight from the source',
+    )
+    wallpaper = Wallpaper(amount="ITEM_1", uuid=customer.id, wallpaper_id=1)
     return render_template('charge.html', amount=amount)
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True)
 
